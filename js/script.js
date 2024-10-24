e1e7289d92b08213999732cd0a76ef875bc86ca0
Isaac Yuri <isaacyuri22@gmail.com>
2024-10-24 16:24:36 -0300

    :recycle: refactor: código js refatorado

--- js/script.js
+++ js/script.js
@@ -2,39 +2,41 @@ const num1 = document.getElementById("n1");
 const num2 = document.getElementById("n2");
 const selecao = document.querySelector("select#operacao");
 const resultado = document.getElementById("res");
+const botao = document.getElementById("botao");
+
+botao.addEventListener("click", (e) => {
+  fazerOperacao();
+});
 
 const operacoes = {
   somar: (n1, n2) => n1 + n2,
-  subrair: (n1, n2) => n1 - n2,
+  subtrair: (n1, n2) => n1 - n2,
   dividir: (n1, n2) => n1 / n2,
   multiplicar: (n1, n2) => n1 * n2,
 };
 
-function calcular() {
+function calcular(operacao, a, b) {
+  return operacao(a, b);
+}
+
+function fazerOperacao() {
+  let a = parseInt(num1.value);
+  let b = parseInt(num2.value);
+
+  const { somar, dividir, multiplicar, subtrair } = operacoes;
+
   switch (selecao.value) {
     case "soma":
-      resultado.innerHTML = operacoes.somar(
-        Number(num1.value),
-        Number(num2.value)
-      );
+      resultado.innerHTML = calcular(somar, a, b);
       break;
     case "divisao":
-      resultado.innerHTML = operacoes.dividir(
-        Number(num1.value),
-        Number(num2.value)
-      );
+      resultado.innerHTML = calcular(dividir, a, b);
       break;
     case "subtrair":
-      resultado.innerHTML = operacoes.subrair(
-        Number(num1.value),
-        Number(num2.value)
-      );
+      resultado.innerHTML = calcular(subtrair, a, b);
       break;
     case "multiplica":
-      resultado.innerHTML = operacoes.multiplicar(
-        Number(num1.value),
-        Number(num2.value)
-      );
+      resultado.innerHTML = calcular(multiplicar, a, b);
       break;
     case "escolhe":
       alert("Por favor selecione uma operação");
